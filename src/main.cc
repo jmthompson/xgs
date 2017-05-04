1d323efebd2b84749e1fc040359efa747e3e1499
Joshua M. Thompson <joshua.thompson@gmail.com>
2017-05-03 22:47:38 -0400

    Added boilerplate comments

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,17 @@
+/**
+ * XGS: The Linux GS Emulator
+ * Written and Copyright (C) 1996 - 2016 by Joshua M. Thompson
+ *
+ * You are free to distribute this code for non-commercial purposes
+ * I ask only that you notify me of any changes you make to the code
+ * Commercial use is prohibited without my written permission
+ */
+
+/*
+ * This file contains the startup code that loads the configuration
+ * and instantiates an Emulator instance.
+ */
+
 #include <iostream>
 #include <stdexcept>
 #include <boost/program_options.hpp>
