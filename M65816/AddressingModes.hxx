d1a2dc663969a013eac0186a77aee92d324c4599
Joshua M. Thompson <joshua.thompson@gmail.com>
2017-05-27 20:24:30 -0400

    fix emulation mode direct page wraparounds

--- M65816/AddressingModes.hxx
+++ M65816/AddressingModes.hxx
@@ -1,3 +1,17 @@
+/**
+ * If the CPU is in emulation mode, and the low byte of D is 0x00,
+ * then wrap the address at the DP boundary.
+ */
+uint16_t wrapDirectPage(uint16_t address)
+{
+    if (!(D & 0xFF) && StackOffset) {
+        return (D & 0xFF00) | (address & 0xFF);
+    }
+    else {
+        return address;
+    }
+}
+
 void getAddress_a()
 {
     operand_addr = system->cpuRead(PBR, PC++, INSTR) | (system->cpuRead(PBR, PC++, INSTR) << 8);
@@ -14,7 +28,7 @@ void getAddress_d()
 {
     checkDirectPageAlignment();
 
-    operand_addr = D + system->cpuRead(PBR, PC++, INSTR);
+    operand_addr = wrapDirectPage(D + system->cpuRead(PBR, PC++, INSTR));
     operand_bank = 0;
 }
 
@@ -23,8 +37,10 @@ void getAddress_dix()
     checkDirectPageAlignment();
 
     uint16_t tmp = D + system->cpuRead(PBR, PC++, INSTR);
+    uint8_t lo = system->cpuRead(0, wrapDirectPage(tmp), OPADDR);
+    uint8_t hi = system->cpuRead(0, wrapDirectPage(tmp + 1), OPADDR);
 
-    operand_addr = system->cpuRead(0, tmp, OPADDR) | (system->cpuRead(0, tmp + 1, OPADDR) << 8);
+    operand_addr = lo | (hi << 8);
     operand_bank = DBR;
 
     tmp = operand_addr;
@@ -50,29 +66,39 @@ void getAddress_dixl()
     if (operand_addr < tmp) operand_bank++;
 }
 
+// (DIRECT,X)
 void getAddress_dxi()
 {
     uint16_t tmp = D + X + system->cpuRead(PBR, PC++, INSTR);
 
     checkDirectPageAlignment();
 
-    operand_addr = system->cpuRead(0, tmp, OPADDR) | (system->cpuRead(0, tmp + 1, OPADDR) << 8);
+    uint8_t lo = system->cpuRead(0, wrapDirectPage(tmp) , OPADDR);
+    uint8_t hi = system->cpuRead(0, wrapDirectPage(tmp + 1), OPADDR);
+
+    operand_addr = lo | (hi << 8);
     operand_bank = DBR;
 }
 
+// DIRECT,X
 void getAddress_dxx()
 {
     checkDirectPageAlignment();
 
-    operand_addr = D + X + system->cpuRead(PBR, PC++, INSTR);
+    uint8_t loc = system->cpuRead(PBR, PC++, INSTR);
+
+    operand_addr = wrapDirectPage(D + loc + X);
     operand_bank = 0;
 }
 
+// DIRECT,Y
 void getAddress_dxy()
 {
     checkDirectPageAlignment();
 
-    operand_addr = D + Y + system->cpuRead(PBR, PC++, INSTR);
+    uint8_t loc = system->cpuRead(PBR, PC++, INSTR);
+
+    operand_addr = wrapDirectPage(D + loc + Y);
     operand_bank = 0;
 }
 
@@ -157,13 +183,17 @@ void getAddress_ail()
     operand_bank = system->cpuRead(0, tmp + 2, OPADDR);
 }
 
+// (DIRECT)
 void getAddress_di()
 {
     uint16_t tmp = D + system->cpuRead(PBR, PC++, INSTR);
 
     checkDirectPageAlignment();
 
-    operand_addr = system->cpuRead(0, tmp, OPADDR) | (system->cpuRead(0, tmp + 1, OPADDR) << 8);
+    uint8_t lo = system->cpuRead(0, tmp, OPADDR);
+    uint8_t hi = system->cpuRead(0, wrapDirectPage(tmp + 1), OPADDR);
+
+    operand_addr = lo | (hi << 8);
     operand_bank = DBR;
 }
 
